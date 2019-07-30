80e684ad80416de91da11cdf0f07487e077aec0e
Anesh Parvatha <anesh@cybereyelabs.io>
2019-07-30 18:12:50 +0530

    [Refactor State] Refactor todostore test cases

--- src/stores/TodoStore/TodoStore.test.js
+++ src/stores/TodoStore/TodoStore.test.js
@@ -6,29 +6,24 @@ describe('TodoStore', () => {
   let todoStore
   beforeEach(() => {
     todoStore = new TodoStore()
-    return todoStore
-  })
-  it('should add a todo to list of todo items', () => {
-    expect(todoStore.todos.length).toBe(0)
-    todoStore.addTodo('Learn TDD')
-    expect(todoStore.todos.length).toBe(1)
-  })
-
-  it('should delete the given todo from list of todo items', () => {
     todoStore.addTodo('Learn Jest')
     todoStore.addTodo('Learn RTL')
     todoStore.addTodo('Learn TDD')
+    return todoStore
+  })
+  it('should add a todo to list of todo items', () => {
+    expect(todoStore.todos.length).toBe(3)
+    todoStore.addTodo('Learn Clean code')
+    expect(todoStore.todos.length).toBe(4)
+  })
 
+  it('should delete the given todo from list of todo items', () => {
     expect(todoStore.todos.length).toBe(3)
     todoStore.deleteTodo(todoStore.todos[0])
     expect(todoStore.todos.length).toBe(2)
   })
 
   it('should clear completed todo items', () => {
-    todoStore.addTodo('Learn Jest')
-    todoStore.addTodo('Learn RTL')
-    todoStore.addTodo('Learn TDD')
-
     expect(todoStore.todos.length).toBe(3)
     todoStore.todos[0].toggleCompleted()
     todoStore.todos[2].toggleCompleted()
@@ -41,10 +36,6 @@ describe('TodoStore', () => {
   })
 
   it('should set the applied filter value as ALL', () => {
-    todoStore.addTodo('Learn Jest')
-    todoStore.addTodo('Learn RTL')
-    todoStore.addTodo('Learn TDD')
-
     todoStore.setAppliedFilter(todoFilterTypes.all)
     expect(todoStore.appliedFilter).toBe(todoFilterTypes.all)
     expect(todoStore.filteredTodos.length).toBe(3)
