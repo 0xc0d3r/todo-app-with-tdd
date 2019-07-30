c2e3959404af8a4bc8dfaefdce130950c9c7fda0
Anesh Parvatha <anesh@cybereyelabs.io>
2019-07-30 19:54:35 +0530

    [Red State] Implement all filter handler

--- src/components/TodoApp/TodoFooter/TodoFooter.test.js
+++ src/components/TodoApp/TodoFooter/TodoFooter.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
+import { todoFilterTypes } from '../../../constants/TodoAppConstants'
 import TodoStore from '../../../stores/TodoStore'
 
 import TodoFooter from './index'
@@ -13,5 +14,14 @@ describe('TodoFooter', () => {
     const clearCompletedBtn = getByTestId('clear-completed-btn')
     fireEvent.click(clearCompletedBtn)
     expect(todoStore.clearCompleted).toBeCalledTimes(1)
+  })
+
+  it('should set applied filter as ALL after clicking on All button', () => {
+    const todoStore = new TodoStore()
+    jest.spyOn(todoStore, 'setAppliedFilter')
+    const { getByTestId } = render(<TodoFooter todoStore={todoStore} />)
+    const allFilterBtn = getByTestId('all-btn')
+    fireEvent.click(allFilterBtn)
+    expect(todoStore.setAppliedFilter).toBeCalledWith(todoFilterTypes.all)
   })
 })
