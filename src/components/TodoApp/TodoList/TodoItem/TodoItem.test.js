fc05d5d4b08eb92fc29f7394470381b5aa1dffb1
Anesh Parvatha <anesh@cybereyelabs.io>
2019-07-30 15:38:56 +0530

    [Red State] Implement delete todo

--- src/components/TodoApp/TodoList/TodoItem/TodoItem.test.js
+++ src/components/TodoApp/TodoList/TodoItem/TodoItem.test.js
@@ -21,4 +21,21 @@ describe('TodoItem', () => {
     expect(checkbox.checked).toBe(true)
     expect(todo.toggleCompleted).toHaveBeenCalledTimes(1)
   })
+
+  it('should delete the todo on click delete button', () => {
+    const newTodo = {
+      id: Date.now(),
+      description: 'Learn TDD',
+      completed: false
+    }
+    const todo = new Todo(newTodo)
+    const onDeleteTodo = jest.fn()
+    const { getByTestId } = render(
+      <TodoItem todo={todo} onDeleteTodo={onDeleteTodo} />
+    )
+
+    const deleteBtn = getByTestId('delete-btn')
+    fireEvent.click(deleteBtn)
+    expect(onDeleteTodo).toBeCalledWith(todo)
+  })
 })
