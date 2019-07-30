0ad8cd144f026102684c66c35caeabab990e23ad
Anesh Parvatha <anesh@cybereyelabs.io>
2019-07-30 10:50:53 +0530

    [Red State] Implement add todo

--- src/stores/TodoStore/TodoStore.test.js
+++ src/stores/TodoStore/TodoStore.test.js
@@ -1,5 +1,10 @@
+import TodoStore from './index'
+
 describe('TodoStore', () => {
   it('should add a todo to list of todo items', () => {
-    const todoStore = new todoStore()
+    const todoStore = new TodoStore()
+    expect(todoStore.todos.length).toBe(0)
+    todoStore.addTodo('Learn TDD')
+    expect(todoStore.todos.length).toBe(1)
   })
 })
